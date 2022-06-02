65943cef34dca378893d9dd6aec1817d8b43c455
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2022-06-01 23:04:06 -0700

    Add Home and Update Main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Main from '../Components/Main'
 import Navbar from '../Components/Navbar'
+import Row from '../Components/Row'
+import request from '../Requests'
 
 const Home = () => {
   return (
     <>
     <Main />
+    <Row title= 'Up Coming' fetchURL={request.requestUpcoming}/>
+    <Row title= 'Popular' fetchURL={request.requestPopular}/>
+    <Row title= 'Trending' fetchURL={request.requestTrending}/>
+    <Row title= 'Horror' fetchURL={request.requestHorror}/>
+    <Row title= 'Top Rated' fetchURL={request.requestTopRated}/>
     </>
   )
 }
